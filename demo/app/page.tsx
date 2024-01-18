10a9a65f7fca065a8b2382797060cb257209179f
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-01-18 02:22:31 -0500

    feat: add initial config

---
+++ demo/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+
+  return (
+    <div className="min-h-dvh prose w-full mx-auto">
+      <div className="prose flex flex-col items-center justify-center">
+        <h1>Homepage</h1>
+      </div>
+    </div>
+  )
+}
