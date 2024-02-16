e50c550ff390482c9efa3daacb7c4864bb731101
victor <52110451+cs50victor@users.noreply.github.com>
2024-02-16 12:38:18 -0500

    feat: setup bevy_remote_asset (#19)
    * refactor: remote_asset dir and use .workspace syntax
    
    * refactor: setup bevy_remote_asset

---
+++ crates/bevy_remote_asset/src/lib.rs
@@ -0,0 +1,7 @@
+// derived from https://github.com/johanhelsing/bevy_web_asset
+
+mod web_asset_plugin;
+mod web_asset_source;
+
+pub use web_asset_plugin::WebAssetPlugin;
+pub use web_asset_source::WebAssetReader;
---
+++ crates/bevy_remote_asset/src/web_asset_plugin.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+
+use crate::web_asset_source::*;
+use bevy::asset::io::AssetSource;
+
+/// Add this plugin to bevy to support loading http and https urls.
+///
+/// Needs to be added before Bevy's `DefaultPlugins`.
+///
+/// # Example
+///
+/// ```no_run
+/// # use bevy::prelude::*;
+/// # use bevy_web_asset::WebAssetPlugin;
+///
+/// let mut app = App::new();
+///
+/// app.add_plugins((
+///     WebAssetPlugin::default(),
+///     DefaultPlugins
+/// ));
+/// ```
+#[derive(Default)]
+pub struct WebAssetPlugin;
+
+impl Plugin for WebAssetPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_asset_source(
+            "https",
+            AssetSource::build().with_reader(|| Box::new(WebAssetReader::Https)),
+        );
+    }
+}
