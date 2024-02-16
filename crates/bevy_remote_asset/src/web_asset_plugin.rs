94fbf554131f149a9f5b8f37fb97dffc2c589f76
victor <52110451+cs50victor@users.noreply.github.com>
2024-02-16 13:02:12 -0500

    refactor: use ubuntu for testing (#20)
    * refactor: use ubuntu for testing
    
    * fix: docs import comment

--- crates/bevy_remote_asset/src/web_asset_plugin.rs
+++ crates/bevy_remote_asset/src/web_asset_plugin.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::web_asset_source::*;
 use bevy::asset::io::AssetSource;
 
-/// Add this plugin to bevy to support loading http and https urls.
+/// Add this plugin to bevy to support loading https urls.
 ///
 /// Needs to be added before Bevy's `DefaultPlugins`.
 ///
@@ -11,7 +11,7 @@ use bevy::asset::io::AssetSource;
 ///
 /// ```no_run
 /// # use bevy::prelude::*;
-/// # use bevy_web_asset::WebAssetPlugin;
+/// # use bevy_remote_asset::WebAssetPlugin;
 ///
 /// let mut app = App::new();
 ///
