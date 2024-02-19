8b18b66b7792d9c30945252c9717ec2c678f2065
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-02-19 11:25:54 -0500

    refactor: load remote splat file

--- new_media/src/asset.rs
+++ new_media/src/asset.rs
@@ -13,37 +13,33 @@ use bevy_panorbit_camera::PanOrbitCamera;
 
 use bevy_gaussian_splatting::{GaussianCloud, GaussianSplattingBundle};
 
-pub fn setup_gaussian_cloud(
+pub fn setup_gaussian_cloud(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let cloud = asset_server.load::<GaussianCloud>("https://huggingface.co/datasets/cs50victor/splats/resolve/main/train/point_cloud/iteration_7000/point_cloud.gcloud");
+
+    log::info!("loading {}", cloud.id());
+
+    let gs = GaussianSplattingBundle { cloud, ..default() };
+    commands.spawn((gs, Name::new("gaussian_cloud")));
+}
+
+pub fn setup_camera(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut gaussian_assets: ResMut<Assets<GaussianCloud>>,
     mut scene_controller: ResMut<bevy_headless::SceneInfo>,
     mut images: ResMut<Assets<Image>>,
     export_sources: ResMut<Assets<ImageExportSource>>,
 ) {
-    // let remote_file = Some("https://huggingface.co/datasets/cs50victor/splats/resolve/main/train/point_cloud/iteration_7000/point_cloud.gcloud");
-    // TODO: figure out how to load remote files later
-    let splat_file = "splats/garden/point_cloud/iteration_7000/point_cloud.gcloud";
-    log::info!("loading {}", splat_file);
-    // let cloud = asset_server.load(splat_file.to_string());
-
-    let cloud = gaussian_assets.add(GaussianCloud::test_model());
-
-    let render_target = bevy_headless::setup_render_target(
+    let target = bevy_headless::setup_render_target(
         &mut commands,
         &mut images,
         &mut scene_controller,
         export_sources,
     );
 
-    let gs = GaussianSplattingBundle { cloud, ..default() };
-    commands.spawn((gs, Name::new("gaussian_cloud")));
-
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
             tonemapping: Tonemapping::None,
-            camera: Camera { target: render_target, ..default() },
+            camera: Camera { target, ..default() },
             ..default()
         },
         PanOrbitCamera {
