dae15914b6bfa2f19f618a991228057a5c62e481
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-02-21 14:06:36 -0500

    refactor: add fal.ai

---
+++ www/app/fal/page.tsx
@@ -0,0 +1,129 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import * as fal from "@fal-ai/serverless-client"
+import dynamic from "next/dynamic";
+import Image from 'next/image'
+
+fal.config({
+  proxyUrl: "/api/fal/proxy",
+})
+
+const seed = Math.floor(Math.random() * 100000)
+
+const baseArgs = {
+  sync_mode: true,
+  strength: .99,
+  seed
+}
+
+// Since client components get prerenderd on server as well hence importing 
+// the excalidraw stuff dynamically with ssr false
+
+const InputCanvas = dynamic(
+    async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+    {
+      ssr: false,
+    },
+);
+
+
+  
+
+export default function Home() {
+  const [input, setInput] = useState('masterpice, best quality, A cinematic shot of a baby raccoon wearing an intricate italian priest robe')
+  const [image, setImage] = useState(null)
+  const [sceneData, setSceneData] = useState<any>(null)
+  const [excalidrawAPI, setExcalidrawAPI] = useState<any>(null)
+  const [_appState, setAppState] = useState<any>(null)
+  const [excalidrawExportFns, setexcalidrawExportFns] = useState<any>(null)
+
+    useEffect(() => {
+        import('@excalidraw/excalidraw').then((module) =>
+            setexcalidrawExportFns({
+            exportToBlob: module.exportToBlob,
+            serializeAsJSON: module.serializeAsJSON
+            })
+        );
+    }, []);
+
+  const { send } = fal.realtime.connect('110602490-sdxl-turbo-realtime', {
+    connectionKey: 'realtime-nextjs-app',
+    onResult(result) {
+      if (result.error) return
+      setImage(result.images[0].url)
+    }
+  })
+
+  async function getDataUrl(appState = _appState) {
+    const elements = excalidrawAPI.getSceneElements()
+    if (!elements || !elements.length) return
+    const blob = await excalidrawExportFns.exportToBlob({
+      elements,
+      exportPadding: 0,
+      appState,
+      quality: 0.5,
+      files: excalidrawAPI.getFiles(),
+      getDimensions: () => { return {width: 450, height: 450}}
+    })
+    return await new Promise(r => {let a=new FileReader(); a.onload=r; a.readAsDataURL(blob)}).then((e:any) => e.target.result)
+  }
+
+  return (
+    <main className="p-12 max-w-screen-xl mx-auto">
+      <p className="text-xl mb-2">Fal SDXL Turbo</p>
+      <input
+        className='border rounded-lg p-2 w-full mb-2'
+        value={input}
+        onChange={async (e) => {
+          setInput(e.target.value)
+          let dataUrl = await getDataUrl()
+          send({
+            ...baseArgs,
+            prompt: e.target.value,
+            image_url: dataUrl
+          })
+        }}
+      />
+      <div className='flex space-x-4'>
+        <div className="w-[550px] h-[570px]">
+            <div className='border w-full h-full rounded-xl p-1'>
+                <InputCanvas 
+                    excalidrawAPI={(api:any)=> setExcalidrawAPI(api)}
+                    onChange={async (elements:any, appState:any) => {
+                        const newSceneData = excalidrawExportFns.serializeAsJSON(
+                            elements,
+                            appState,
+                            excalidrawAPI.getFiles(),
+                            'local'
+                        )
+                        if (newSceneData !== sceneData) {
+                            setAppState(appState)
+                            setSceneData(newSceneData)
+                            let dataUrl = await getDataUrl(appState)
+                            send({
+                            ...baseArgs,
+                            image_url: dataUrl,
+                            prompt: input,
+                            })
+                        }
+                    }}
+                />
+            </div>
+        </div>
+        {
+          image && (
+            <div className='border rounded-lg p-1'>
+                <Image
+                  src={image}
+                  width={550}
+                  height={550}
+                  alt='fal image'
+                />
+            </div>
+          )
+        }
+      </div>
+    </main>
+  )
+}
