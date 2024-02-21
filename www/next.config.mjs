5b4e8f89b55fb769f66f49e47bb295e2e1c514fa
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-02-21 15:22:41 -0500

    feat: add remotion

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -5,6 +5,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  reactStrictMode: true,
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Workaround for the following issue:
+    // https://github.com/aws-amplify/amplify-js/issues/11030#issuecomment-1598207365
+
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ }),
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^@aws-sdk\/signature-v4-crt$/,
+        })
+      );
+    return config;
+  },
   // experimental: {
   //   ppr: true
   // },
